5e7ce3208988769da9476b44357f6a3d14d71d22
shiv-anya <sg90883@gmail.com>
2026-07-13 16:04:25 +0530

    added full link

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL + "/api" || "http://localhost:5000/api",
 });
 
 export async function getTasks(status) {
